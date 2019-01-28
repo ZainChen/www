f43c770cf94a391609c507537a1e0487fab08159
ZainChen <2384439266@qq.com>
2019-01-28 15:32:23 +0800

    update talk

--- talk/script/movie.js
+++ talk/script/movie.js
@@ -4,12 +4,12 @@ var btn_grey = document.getElementById("btn_grey");
 btn_grey.onclick = function() {
   var form1 = document.getElementById("form1");
   if(form1.txt_title.value == "") {  //公告标题为空则弹出警告框
-    alert("请输入公告标题!");
+    alert("请输入昵称!");
     form1.txt_title.focus();  //获得标题出入框焦点
     return false;
   }
   if(form1.txt_content.value == "") {  //公告内容为空则弹出警告框
-    alert("请输入公告内容!");
+    alert("请输入留言内容!");
     form1.txt_content.focus();  //获得公告内容框焦点
     return false;
   }
@@ -36,6 +36,37 @@ submit3.onclick = function() {
   from002.style.height = "0%";
   from002.style.transition = "1s";
 }
+//删除按钮功能(显示是否删除)
+var delbtnNum = 1;
+while(1) {
+  var delbtn = document.getElementById("delbtn"+delbtnNum);
+  if(delbtn == null) {
+    break;
+  }
+  delbtn.onclick = (function(i) {
+    return function() {
+      var delyn = document.getElementById("delyn"+i);
+      delyn.style.display = "inline";
+    }
+  })(delbtnNum);
+  delbtnNum++;
+}
+//隐藏是否删除按钮
+var delnNum = 1;
+while(1) {
+  var deln = document.getElementById("deln"+delnNum);
+  if(deln == null) {
+    break;
+  }
+  deln.onclick = (function(i) {
+    return function() {
+      var delyn = document.getElementById("delyn"+i);
+      delyn.style.display = "none";
+    }
+  })(delnNum);
+  delnNum++;
+}
+
 //======刷新页面功能=======================
 //单击刷新按钮重新加载页面
 var refresh_info = document.getElementById("refresh_info");
@@ -55,7 +86,7 @@ btn_search.onclick = function() {  //验证搜索表单里的内容是否为空
 }
 //====修改数据功能页面的刷新即调出修改框功能==========
 window.onload = function() {
-  var mdy = document.getElementById("mdy");
+  //var mdy = document.getElementById("mdy");
   var url = window.location.search;  //获取当前页面路径问号及问号以后的字符串
   // alert(url.length);
   if(url[4] != '0' && url != "") {
