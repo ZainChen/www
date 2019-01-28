0ee5558938606f34179971f0c1582f1fda1ea48c
ZainChen <2384439266@qq.com>
2019-01-28 17:25:38 +0800

    update talk

--- talk/php/refresh_movie.php
+++ talk/php/refresh_movie.php
@@ -9,6 +9,7 @@
     $keyword = $_GET['txt_keyword'];
   }
   mysqli_query($conn, "truncate table tb_talk_look") or die(("[SHOW0]数据表访问错误,请检查该表是否存在".mysqli_error()));
+  mysqli_query($conn, "truncate table tb_talk") or die(("[SHOWaaaaaaaaaaa]数据表访问错误,请检查该表是否存在".mysqli_error()));
   //mysqli_query($conn, "INSERT INTO tb_talk_look(id,nickname,content,createtime) SELECT id,nickname,content,createtime FROM tb_talk where nickname like '%$keyword%' or content like '%$keyword%' or createtime like '%$keyword%'") or die(("[SHOW1]数据表访问错误,请检查该表是否存在".mysqli_error()));
   mysqli_query($conn, "INSERT INTO tb_talk_look(id,nickname,content,createtime) SELECT id,nickname,content,createtime FROM tb_talk where nickname like '%$keyword%' or content like '%$keyword%' or createtime like '%$keyword%'") or die(("[SHOW1]数据表访问错误,请检查该表是否存在".mysqli_error()));
 
