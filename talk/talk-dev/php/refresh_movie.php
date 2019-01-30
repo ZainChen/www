cd9dded7ce03950cfe23b1a240786792b2a040e4
ZainChen <2384439266@qq.com>
2019-01-30 15:00:32 +0800

    update talk 'add Header'

--- talk/talk-dev/php/refresh_movie.php
+++ talk/talk-dev/php/refresh_movie.php
@@ -25,6 +25,19 @@
       <meta charset="UTF-8">
       <title>Talk</title>
       <link rel="stylesheet" href="../style/movie.css" />
+      <!-- home style -->
+      <link href="../../home-res/css/bootstrap.css" rel="stylesheet">
+      <link href="../../home-res/css/bootstrap-theme.css" rel="stylesheet">
+      <link href="../../home-res/css/block_grid_bootstrap.css" rel="stylesheet">
+      <link href="../../home-res/css/owl.carousel.css" rel="stylesheet">
+      <link href="../../home-res/css/owl.theme.css" rel="stylesheet">
+      <link href="../../home-res/css/animate.min.css" rel="stylesheet" />
+      <link href="../../home-res/css/jquery.circliful.css" rel="stylesheet" />
+      <link href="../../home-res/css/select2.css" rel="stylesheet">
+      <link href="../../home-res/css/slicknav.css" rel="stylesheet" />
+      <link href="../../home-res/css/style.css" rel="stylesheet">
+      <link href="../../home-res/css/user-style.css" rel="stylesheet">
+      <!-- End of home style -->
     </head>
     <body>
       <div id="head0">
@@ -174,6 +187,23 @@
       </div>
     </body>
     <script type="text/javascript" src="../script/movie.js"></script>
+    <!-- home script -->
+    <script src="../../home-res/js/jquery.min.js"></script>
+    <script src="../../home-res/js/bootstrap.min.js"></script>
+    <script src="../../home-res/js/hoverIntent.js"></script>
+    <script src="../../home-res/js/superfish.min.js"></script>
+    <script src="../../home-res/js/owl.carousel.js"></script>
+    <script src="../../home-res/js/wow.min.js"></script>
+    <script src="../../home-res/js/waypoints.min.js"></script>
+    <script src="../../home-res/js/jquery.slicknav.min.js"></script>
+    <script src="../../home-res/js/retina.min.js"></script>
+    <script src="../../home-res/js/select2.js"></script>
+    <script src="../../home-res/js/counterup.min.js"></script>
+    <script src="../../home-res/js/waves.js"></script>
+    <script src="../../home-res/js/jquery.countdown.js"></script>
+    <script src="../../home-res/js/custom.js"></script>
+    <script src="../../home-res/js/user-script.js"></script>
+    <!-- End of home script -->
   </html>
 <?php
   mysqli_free_result($sql);  //关闭结果集
