b89b61135df12a9f441ece2b722ecf31b4b11e5a
ZainChen <2384439266@qq.com>
2019-01-30 20:12:05 +0800

    update user-script

--- home-res/js/user-script.js
+++ home-res/js/user-script.js
@@ -102,19 +102,19 @@ $(document).on("ready", function(e) {
 });
 
 //<!------------------------------------------赞助------------------------------------------>
-new tctip({
-    top: '20%',
-    button: {
-        id: 3,
-        type: 'zanzhu'
-    },
-    list: [
-        {
-            type: 'alipay',
-            qrImg: 'https://zainzy.com/home-res/images/o_weMoney.png'
-        }, {
-            type: 'wechat',
-            qrImg: 'https://zainzy.com/home-res/images/o_weMoney.png'
-        }
-    ]
-}).init();
+//new tctip({
+//    top: '20%',
+//    button: {
+//        id: 3,
+//        type: 'zanzhu'
+//    },
+//    list: [
+//        {
+//            type: 'alipay',
+//            qrImg: 'https://zainzy.com/home-res/images/o_weMoney.png'
+//        }, {
+//            type: 'wechat',
+//            qrImg: 'https://zainzy.com/home-res/images/o_weMoney.png'
+//        }
+//    ]
+//}).init();
